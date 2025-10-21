f2ffd1711e986dc35e6e110468c0b46a00ace44d
Ange-Pascal <angeakoumoua@gmail.com>
2025-10-21 08:18:44 +0000

    Les variables et les types scalaires

--- index.py
+++ index.py
@@ -1,5 +1,21 @@
-nom = "Ange-Pascal" 
-age = 30 
+#Leçon 1
+# Ecris une ligne de code pour l'ordinateur
 
-print(f"Bonjour {nom}, tu as {age} ans")
-print("Bonjour " + nom, + "tu as " + str(age) + "ans")
+
+print("Je m'appelle Alex et j'apprends Python !")
+
+# Leçon 2 Les variables 
+
+# Quel est le reusltat ici ? 
+
+#Leçon 3 les types 
+
+a = 10 
+b = 3.14 
+is_valid = True
+vide = None 
+
+print(f"La valeur de a est {a} et son type est {type(a)}")
+print(f"La valeur de b est {b} et son type est {type(b)}")
+print(f"La valeur de is_valid est {is_valid} et son type est {type(is_valid)}")
+print(f"La valeur de vide est {vide} et son type est {type(vide)}")
