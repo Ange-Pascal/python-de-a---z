117aa314cff6723a94ff1aa94d350dd0dd190769
Ange-Pascal <angeakoumoua@gmail.com>
2025-10-21 08:56:15 +0000

    Les chaines de caractères

--- index.py
+++ index.py
@@ -19,3 +19,14 @@ print(f"La valeur de a est {a} et son type est {type(a)}")
 print(f"La valeur de b est {b} et son type est {type(b)}")
 print(f"La valeur de is_valid est {is_valid} et son type est {type(is_valid)}")
 print(f"La valeur de vide est {vide} et son type est {type(vide)}")
+
+# Leçon 4 : Les chaines de caractères 
+
+message = "Bonjour" 
+text = """Je suis plus que focus sur Dieu et sur mes objectifs
+Je ne vais plus au Damier
+ça ne s'inscrit pas dans mes objectifs"""
+print(len(message))
+print(message.find(str(a))) 
+
+print(text)
